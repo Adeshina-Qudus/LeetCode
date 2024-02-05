de5766d04e0ce872fbe98c80882b387f5229d34f
Adeshina Qudus <qudusa55@gmail.com>
2024-02-05 10:13:58 +0100

    commit

--- src/leetCode/LowestInTwoArray.java
+++ src/leetCode/LowestInTwoArray.java
@@ -7,10 +7,10 @@ public class LowestInTwoArray {
 
     public static int lowestInTwoArray(int[] inputArray, int[] input) {
         ArrayList<Integer> lowestInTwoArray = new ArrayList<>();
-        for (int firstLoop = 0; firstLoop < inputArray.length; firstLoop++){
-            for (int secondLoop = 0; secondLoop < input.length; secondLoop++){
-                if (inputArray[firstLoop] == input[secondLoop]){
-                    lowestInTwoArray.add(inputArray[firstLoop]);
+        for (int firstLoopElement : inputArray) {
+            for (int secondLoopElement : input) {
+                if (firstLoopElement == secondLoopElement) {
+                    lowestInTwoArray.add(firstLoopElement);
                 }
             }
         }
@@ -20,9 +20,9 @@ public class LowestInTwoArray {
     private static int lowest(ArrayList<Integer> lowestInTwoArray) {
         int result = 0 ;
         int smallest = lowestInTwoArray.get(0);
-        for (int loop = 0; loop < lowestInTwoArray.size(); loop++){
-            if (lowestInTwoArray.get(loop) <= smallest){
-                result = lowestInTwoArray.get(loop);
+        for (Integer integer : lowestInTwoArray) {
+            if (integer <= smallest) {
+                result = integer;
             }
         }
         return result;
