8cf00601e04f1074faf4e45c5d05e3193eaed8e3
Adeshina Qudus <qudusa55@gmail.com>
2024-04-03 13:58:27 +0100

    commit

---
+++ test/leetCode/ThreeSumClosestTest.java
@@ -0,0 +1,17 @@
+package leetCode;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class ThreeSumClosestTest {
+
+
+    @Test
+    public void test(){
+        int [] input = {-1,2,1,-4};
+        int output = 2;
+        assertEquals(output,ThreeSumClosest.threeSumClosest(input));
+    }
+
+}
