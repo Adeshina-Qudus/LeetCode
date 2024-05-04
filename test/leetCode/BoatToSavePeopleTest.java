f1c26925eb398978ac94237c182cec2bcc22900b
Adeshina Qudus <qudusa55@gmail.com>
2024-05-04 20:51:30 +0100

    commit

--- test/leetCode/BoatToSavePeopleTest.java
+++ test/leetCode/BoatToSavePeopleTest.java
@@ -13,4 +13,18 @@ public class BoatToSavePeopleTest {
         int limit = 3;
         assertEquals(1, BoatToSavePeople.numberRescueBoat(input,limit));
     }
+
+    @Test
+    public void secondTest() {
+        int[] input = {3, 2, 2, 1};
+        int limit = 3;
+        assertEquals(3, BoatToSavePeople.numberRescueBoat(input, limit));
+    }
+
+    @Test
+    public void thirdTest(){
+        int [] input = {3,5,3,4};
+        int limit = 3;
+        assertEquals(5, BoatToSavePeople.numberRescueBoat(input,limit));
+    }
 }
