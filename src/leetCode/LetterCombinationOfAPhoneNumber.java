446a5dc2e76ad110b99e15624ff9b9e2ef126087
Adeshina Qudus <qudusa55@gmail.com>
2024-05-09 12:46:46 +0100

    LetterCombinationOfAPhoneNumber

--- src/leetCode/LetterCombinationOfAPhoneNumber.java
+++ src/leetCode/LetterCombinationOfAPhoneNumber.java
@@ -1,13 +1,34 @@
 package leetCode;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class LetterCombinationOfAPhoneNumber {
 
     public static List<String> letterCombinationOfAPhoneNumber(String input) {
-
         String [] phoneNumbers = {" "," ","abc","def","ghi","jkl","mno","pqrs","tuv","wxyz"};
-
-
+        List<String> result = new ArrayList<>();
+        if (input.isEmpty()){
+            return result;
+        }
+        String takingNumbers;
+        int firstNumber = Integer.parseInt(String.valueOf(input.charAt(0)));
+        int secondNumber = 0;
+        if (input.length() > 1){
+            secondNumber = Integer.parseInt(String.valueOf(input.charAt(1)));
+            for (int count = 0 ; count < phoneNumbers[firstNumber].length(); count++){
+                for (int counter = 0; counter < phoneNumbers[secondNumber].length() ; counter++){
+                    takingNumbers = String.valueOf(phoneNumbers[firstNumber].charAt(count) ).
+                            concat(String.valueOf(phoneNumbers[secondNumber].charAt(counter)));
+                    result.add(takingNumbers);
+                }
+            }
+        }else {
+            for (int count = 0 ; count < phoneNumbers[firstNumber].length(); count++){
+                takingNumbers = String.valueOf(phoneNumbers[firstNumber].charAt(count));
+                result.add(takingNumbers);
+            }
+        }
+        return result;
     }
 }
--- test/leetCode/LetterCombinationOfAPhoneNumberTest.java
+++ test/leetCode/LetterCombinationOfAPhoneNumberTest.java
@@ -2,6 +2,7 @@ package leetCode;
 
 import org.junit.jupiter.api.Test;
 
+import java.util.Arrays;
 import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.assertArrayEquals;
@@ -11,9 +12,19 @@ public class LetterCombinationOfAPhoneNumberTest {
 
     @Test
     public void firstTest(){
-
         String input = "23";
         String [] result = {"ad","ae","af","bd","be","bf","cd","ce","cf"};
         assertArrayEquals(result,LetterCombinationOfAPhoneNumber.letterCombinationOfAPhoneNumber(input).toArray());
+    }
+    @Test
+    public void secondTest(){
+        String input = "2";
+        String [] result = {"a","b","c"};
+        assertArrayEquals(result,LetterCombinationOfAPhoneNumber.letterCombinationOfAPhoneNumber(input).toArray());
+    }
+    @Test
+    public void thirdTest(){
+        String input = "";
+        System.out.println(Arrays.toString(LetterCombinationOfAPhoneNumber.letterCombinationOfAPhoneNumber(input).toArray()));
     }
 }
