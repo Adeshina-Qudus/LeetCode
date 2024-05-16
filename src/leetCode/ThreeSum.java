6bba6964b53d415c31b00270834121de0e5d9e1e
Adeshina Qudus <qudusa55@gmail.com>
2024-05-16 11:15:40 +0100

    threeSum

---
+++ src/leetCode/ThreeSum.java
@@ -0,0 +1,10 @@
+package leetCode;
+
+import java.util.List;
+
+public class ThreeSum {
+
+    public static List<List<Integer>> threeSum(int[] input) {
+
+    }
+}
---
+++ test/leetCode/ThreeSumTest.java
@@ -0,0 +1,16 @@
+package leetCode;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertArrayEquals;
+
+public class ThreeSumTest {
+
+
+    @Test
+    public void firstTest(){
+        int [] input = {-1,0,1,2,-1,-4};
+        int[][] output = {{-1,-1,2},{-1,0,1}};
+        assertArrayEquals(output, ThreeSum.threeSum(input).toArray());
+    }
+}
