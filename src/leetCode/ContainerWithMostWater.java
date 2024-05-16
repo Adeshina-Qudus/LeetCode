400151377b9af404f41f92e0984d430ee83e3875
Adeshina Qudus <qudusa55@gmail.com>
2024-05-16 10:32:13 +0100

    ContainerWithMostWater

--- src/leetCode/ContainerWithMostWater.java
+++ src/leetCode/ContainerWithMostWater.java
@@ -1,4 +1,33 @@
 package leetCode;
 
+import java.util.Arrays;
+
 public class ContainerWithMostWater {
+
+
+    public static int containerWithMostWater(int[] input) {
+        int distance ;
+        int []maximumArray = new int[input.length];
+        int result = 0;
+        int maximum = 0;
+        int minimum ;
+        int leftPointer = 0 ;
+        int rightPointer = input.length - 1 ;
+        for (int count = 0 ; count < input.length; count++){
+            if (input[leftPointer] < input[rightPointer]){
+                minimum = input[leftPointer];
+                distance = rightPointer - leftPointer;
+                leftPointer++;
+            }else {
+                minimum = input[rightPointer];
+                distance = rightPointer - leftPointer;
+                rightPointer--;
+            }
+            maximum = distance * minimum;
+            maximumArray[count] =maximum;
+        }
+         result = Arrays.stream(maximumArray).max().getAsInt();
+        return result;
+    }
+
 }
