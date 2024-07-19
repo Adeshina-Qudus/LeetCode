678d204fbe0fdc0e154b5229e420f5d50fc934e0
Adeshina Qudus <qudusa55@gmail.com>
2024-07-19 11:42:16 +0100

    commit

--- test/leetCode/BaseBallGameTest.java
+++ test/leetCode/BaseBallGameTest.java
@@ -12,7 +12,6 @@ public class BaseBallGameTest {
 
     @Test
     public void testBaseBallGame() {
-
         String [] elements = {"5","2","C","D","+"};
         int output = 30;
         assertEquals(output,BaseBallGame.baseBallGame(elements));
@@ -21,6 +20,11 @@ public class BaseBallGameTest {
     public void testBaseBallGameTwo() {
         String [] elements = {"5","-2","4","C","D","9","+","+"};
         int output = 27;
+        assertEquals(output,BaseBallGame.baseBallGame(elements));
+    }@Test
+    public void testBaseBallGameThree() {
+        String [] elements = {"1","C"};
+        int output = 0;
         assertEquals(output,BaseBallGame.baseBallGame(elements));
     }
 }
